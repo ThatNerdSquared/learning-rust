fddb638ec7d1b49490048f91e0230bf693b12ce7
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2022-05-27 16:51:54 -0700

    feat: add fibonacci generator

--- chapter-3-exercises/src/main.rs
+++ chapter-3-exercises/src/main.rs
@@ -16,50 +16,69 @@ fn main() {
         .expect("Failed to read line");
 
     match selection.as_str().trim() {
-        "1" => {
-            println!("Input temperature in degrees:");
-            let mut degrees = String::new();
-
-            io::stdin()
-                .read_line(&mut degrees)
-                .expect("Failed to read line");
-            let degrees: i32 = degrees.trim().parse::<i32>().unwrap();
-
-            println!("Input unit (F, C):");
-            let mut unit = String::new();
-
-            io::stdin()
-                .read_line(&mut unit)
-                .expect("Failed to read line");
-
-            match unit.as_str().trim() {
-                "C" => {
-                    println!(
-                        "{} degrees Celsius is: {} F",
-                        degrees.to_string(),
-                        celsius_to_fahrenheit(degrees)
-                    )
-                }
-                "F" => {
-                    println!(
-                        "{} degrees Fahrenheit is: {} C",
-                        degrees.to_string(),
-                        fahrenheit_to_celsius(degrees)
-                    )
-                }
-                _ => {
-                    println!("Unit not found!")
-                }
-            }
-        }
+        "1" => temperature_converter(),
         "2" => {
-            println!("Fibonacci Generator");
+            println!("Which Fibonacci number do you want?");
+            let mut num = String::new();
+            io::stdin()
+                .read_line(&mut num)
+                .expect("Failed to read line");
+
+            let num: i32 = num.trim().parse::<i32>().unwrap();
+            let mut generated_nums: Vec<i32> = Vec::with_capacity(num as usize);
+            generated_nums.push(0);
+            generated_nums.push(1);
+
+            for i in 2..num {
+                let first_index = i - 1;
+                let second_index = i- 2;
+                let new_num = generated_nums[first_index as usize] + generated_nums[second_index as usize];
+                generated_nums.push(new_num);
+            }
+            println!("{}", generated_nums.last().unwrap())
         }
         "3" => {
             println!("Twelve Days of Christmas");
         }
         _ => {
             println!("Operation not found: {}", selection.as_str());
+        }
+    }
+}
+
+fn temperature_converter() {
+    println!("Input temperature in degrees:");
+    let mut degrees = String::new();
+
+    io::stdin()
+        .read_line(&mut degrees)
+        .expect("Failed to read line");
+    let degrees: i32 = degrees.trim().parse::<i32>().unwrap();
+
+    println!("Input unit (F, C):");
+    let mut unit = String::new();
+
+    io::stdin()
+        .read_line(&mut unit)
+        .expect("Failed to read line");
+
+    match unit.as_str().trim() {
+        "C" => {
+            println!(
+                "{} degrees Celsius is: {} F",
+                degrees.to_string(),
+                celsius_to_fahrenheit(degrees)
+            )
+        }
+        "F" => {
+            println!(
+                "{} degrees Fahrenheit is: {} C",
+                degrees.to_string(),
+                fahrenheit_to_celsius(degrees)
+            )
+        }
+        _ => {
+            println!("Unit not found!")
         }
     }
 }
